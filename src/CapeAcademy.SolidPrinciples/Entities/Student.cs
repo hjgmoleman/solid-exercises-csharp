adcefb4a9f65c2225c44f0509ca279d0c3e3964f
Hinke <hinke.moleman@carthago-ict.nl>
2023-12-01 13:37:41 +0100

    feat: add AddBonusAllowance

--- src/CapeAcademy.SolidPrinciples/Entities/Student.cs
+++ src/CapeAcademy.SolidPrinciples/Entities/Student.cs
@@ -6,4 +6,16 @@ public class Student(string emailAddress, Guid universityId)
     public Guid UniversityId { get; private set; } = universityId;
     public int MonthlyEbookAllowance { get; set; } = 0;
     public int CurrentlyBorrowedEbooks { get; private set; } = 0;
+    
+    public void AddBonusAllowance(UniversityPackage package)
+    {
+        if (package == UniversityPackage.Standard)
+        {
+            MonthlyEbookAllowance += 5;
+        }
+        else if (package == UniversityPackage.Premium)
+        {
+            MonthlyEbookAllowance += 10;
+        }
+    }
 }
